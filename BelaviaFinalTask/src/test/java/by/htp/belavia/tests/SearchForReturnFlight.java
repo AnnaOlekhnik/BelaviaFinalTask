94655395d9de21825eaf8e01aa08a0db3bcd44d7
Anna Olekhnik <annaolekhnik@gmail.com>
2018-07-27 15:12:07 +0300

    separated test methods  return and oneway tickets

---
+++ BelaviaFinalTask/src/test/java/by/htp/belavia/tests/SearchForReturnFlight.java
@@ -0,0 +1,22 @@
+package by.htp.belavia.tests;
+
+import org.testng.annotations.Test;
+
+import by.htp.belavia.pages.BookingPage;
+
+public class SearchForReturnFlight extends BaseTest {
+
+	@Test
+	public void searchReturnTicketToRiga(){
+		
+		steps.openBelaviaPage();
+		steps.setDepatureAndDestination("Minsk", "Riga");
+		steps.setOneWayOrReturnTicket("Return");
+		steps.searchReturnFlight();
+		BookingPage page = steps.getResultsOfReturnTicketSearch();
+	
+	       
+		
+	}
+	
+}
